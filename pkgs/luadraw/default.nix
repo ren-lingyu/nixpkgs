3137970deeabb89cc73e35cb7565006c60fed97d
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-01 05:38:46 +0000

    Modified `pkgs/luadraw/default.nix`

--- pkgs/luadraw/default.nix
+++ pkgs/luadraw/default.nix
@@ -1,39 +1,43 @@
-{ stdenv, fetchFromGitHub, lib, pkgs, ... }:
+{ stdenv, fetchFromGitHub, lib }:
 
 stdenv.mkDerivation rec {
   
   pname = "luadraw";
   version = "2.7.0";
-  src = fetchFromGitHub ({
+  src = fetchFromGitHub {
     owner = "pfradin";
     repo = "luadraw";
     rev = "a0713482ef87666fb59a788bae18ba58656e7163";
     fetchSubmodules = false;
     sha256 = "sha256-ERFPsPATl9bhq12vQfMIb5GpU2WXlyTlmw7dAKOir6k=";
-  });
+  };
+  
+  dontBuild = true;
+  dontStrip = true;
+  
+  outputs = [ "out" "tex" "texdoc" ];
   
   passthru = {
     tlType = "tex";
-    outputSpecified = true;
   };
-
-  meta = with lib; {
-    description = "Package ${pname} for LuaLaTeX version ${version}";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    outputsToInstall = [ ];
-  };
-
-  outputs = [ "out" "tex" "texdoc" ];
 
   installPhase = lib.concatStringsSep "\n" [
     "runHook preInstall"
-    "mkdir -p \$out"
-    "mkdir -p \$tex/tex/latex/${pname}/"
+    ""
+    "mkdir -p \$tex/tex/latex/${pname}"
     "cp -r files/* \$tex/tex/latex/${pname}/"
-    "mkdir -p \$texdoc/doc/latex/${pname}/"
+    ""
+    "mkdir -p \$texdoc/doc/latex/${pname}"
     "cp -r doc/* \$texdoc/doc/latex/${pname}/"
+    ""
     "runHook postInstall"
   ];
+
+  meta = with lib; {
+    description = "LuaLaTeX package ${pname} version ${version}";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = [ ];
+  };
   
 }
