19f218dc0a6af81025a971710ada4efa72ae70fc
ren-lingyu <Ren_Lingyu@outlook.com>
2026-06-30 16:03:49 +0800

    refactor(texlive): rename namespace texlivePackages to texlive
    - 将 `pkgs/default.nix` 中命名空间 `texlivePackages` 重命名为 `texlive`.
    - 将目录 `pkgs/texlivePackages/` 重命名为 `pkgs/texlive/`.
    - 更新 `callPackage` 路径以匹配新目录结构.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,9 +3,9 @@
   # example-package = pkgs.callPackage ./example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./some-qt5-package { };
   
-  texlivePackages = {
+  texlive = {
     recurseForDerivations = true;
-    luadraw = pkgs.callPackage ./texlivePackages/luadraw { };
+    luadraw = pkgs.callPackage ./texlive/luadraw { };
   };
 
 }
